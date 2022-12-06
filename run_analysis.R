cb5ac0526e08cb393b62733111f1a42e880679c4
Yusuf Akintunde Azeez <98083840+Aareakin@users.noreply.github.com>
2022-12-06 11:50:05 +0100

    adding labels and deleting some functions

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,3 @@
-filename <- "Coursera_DS3_Final.zip"
-
-if (!file.exists(filename)){
-  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20D
-ataset.zip"
-  download.file(fileURL, filename, method="curl")
-}
-
 
 library(dplyr)
 library(tidyr)
@@ -34,20 +26,21 @@ y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 
-# changing 
+# renaming subjec.t to subject in subject_train
 subject_train <- subject_train %>% rename(subject=subjec.t)
+
+# merging dataset
 Subject <- rbind(subject_train, subject_test)
-
-
-
 Merged_Data <- cbind(Subject, Y, X)
 
-
+# Extracting only the measurements on the mean and SD for each measurement
 TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+
+# Uses descriptive activity names to name the activities in the dataset
 TidyData$code <- activities[TidyData$code, 2]
 
 
-
+# labeling the dataset with descriptive variable names
 names(TidyData)[2] = "activity"
 names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
 names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
@@ -62,17 +55,16 @@ names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRU
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
 
-
-
-
+# creating independent tidy dataset with the average of each variable for each activity and each subject.
 aveData <- TidyData %>%
   group_by(subject, activity) %>%
   summarise_all(funs(mean))
+
+# creating txt file
 write.table(aveData, "aveData.txt", row.name=FALSE)
 
-
+# checking the structure of the dataset
 str(aveData)
 
-
-
+#viewing the newly created dataset
 aveData
